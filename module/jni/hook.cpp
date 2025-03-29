6af6c3e29a9faf16e8b99b92a77c7530a5c1118b
DogEZ <127666673+DdogezD@users.noreply.github.com>
2025-03-30 01:30:24 +0800

    Update hook.cpp

--- module/jni/hook.cpp
+++ module/jni/hook.cpp
@@ -6,41 +6,39 @@ using namespace std;
 
 jstring (*orig_native_get)(JNIEnv *env, jclass clazz, jstring keyJ, jstring defJ);
 
-jstring my_native_get(JNIEnv *env, jclass clazz, jstring keyJ, jstring defJ) {
-    const char *key = env->GetStringUTFChars(keyJ, nullptr);
-    const char *def = env->GetStringUTFChars(defJ, nullptr);
+jstring my_native_get(JNIEnv *env, jclass clazz, jstring keyJ, jstring defJ) { const char *key = env->GetStringUTFChars(keyJ, nullptr); const char *def = env->GetStringUTFChars(defJ, nullptr);
 
-    jstring hooked_result = nullptr;
+jstring hooked_result = nullptr;
 
-    if (strcmp(key, "ro.product.model") == 0) { // ro.product.model=PHN110
-        hooked_result = env->NewStringUTF("PHN110");
-    }
-
-    env->ReleaseStringUTFChars(keyJ, key);
-    env->ReleaseStringUTFChars(defJ, def);
-
-    if (hooked_result != nullptr) {
-        return hooked_result;
-    } else {
-        return orig_native_get(env, clazz, keyJ, defJ);
-    }
+if (strcmp(key, "ro.product.model") == 0) {
+    hooked_result = env->NewStringUTF("PHN110");
 }
 
-void hookSystemProperties(JNIEnv *env, zygisk::Api *api) {
-    LOGD("hook SystemProperties\n");
+env->ReleaseStringUTFChars(keyJ, key);
+env->ReleaseStringUTFChars(defJ, def);
 
-    JNINativeMethod targetHookMethods[] = {
-            {"native_get", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;",
-             (void *) my_native_get},
-    };
-
-    api->hookJniNativeMethods(env, "android/os/SystemProperties", targetHookMethods, 1);
-
-    *(void **) &orig_native_get = targetHookMethods[0].fnPtr;
-
-    LOGD("hook SystemProperties done: %p\n", orig_native_get);
+if (hooked_result != nullptr) {
+    return hooked_result;
+} else {
+    return orig_native_get(env, clazz, keyJ, defJ);
 }
 
-void Hook::hook() {
-    hookSystemProperties(env, api);
 }
+
+void hookSystemProperties(JNIEnv *env, zygisk::Api *api) { LOGD("hook SystemProperties\n");
+
+JNINativeMethod targetHookMethods[] = {
+        {"native_get", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;",
+         (void *) my_native_get},
+};
+
+api->hookJniNativeMethods(env, "android/os/SystemProperties", targetHookMethods, 1);
+
+*(void **) &orig_native_get = targetHookMethods[0].fnPtr;
+
+LOGD("hook SystemProperties done: %p\n", orig_native_get);
+
+}
+
+void Hook::hook() { hookSystemProperties(env, api); }
+
